45aa9d005bf780c3be7a4f63b748eed356cc007c
sojunho <wmswms1020-@naver.com>
2022-06-16 17:07:18 +0900

    2022 0616 오후

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,6 @@ import com.javaex.service.PhoneService;
 import com.javaex.vo.PersonVo;
 
 @Controller
-/* @RequestMapping(value="/guest") */
 public class PhoneController {
 
 	// 필드
@@ -23,61 +23,92 @@ public class PhoneController {
 
 	// 생성자
 
-	// 메소드 gs
+	// 메소드-gs
 
-	// 메소드 일반
+	// 메소드-일반
 
 	// 전화번호 리스트
 	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
 	public String list(Model model) {
-		System.out.println("list");
+		System.out.println("PhoneController>list()");
 
-		// Service를 통해서 personList(주소)를 가져온다
-		// PhoneService phoneService = new PhoneService();
+		// Service를 통해서 personList(주소)을 가져온다
+		// PhoneService phoneService = new PhoneService() X;
 		List<PersonVo> personList = phoneService.getPersonList();
-		System.out.println(personList);
-		// ds데이터 보내기 --> request attribute에 넣는다
+
+		// ds 데이터보내기 -->request attribute에 넣는다
 		model.addAttribute("personList", personList);
 
 		return "list";
+
 	}
 
 	// 전화번호 등록 폼
-
 	@RequestMapping(value = "/writeForm", method = { RequestMethod.GET, RequestMethod.POST })
 	public String writeForm() {
+		System.out.println("PhoneController>writeForm()");
 
-		System.out.println("writeForm");
 		return "writeForm";
 	}
 
-	// 전화번호등록
+	// 전화번호 등록(@ModelAttribute 사용)
 	@RequestMapping(value = "/write", method = { RequestMethod.GET, RequestMethod.POST })
 	public String write(@ModelAttribute PersonVo personVo) {
-		System.out.println("write");
+		System.out.println("PhoneController>write()");
 
-		// 서비스로 저장
-		phoneService.personInsert(personVo);
-
+		// Service를 통해서 저장한다
 		int count = phoneService.personInsert(personVo);
-		
-		System.out.println(count);
+
 		// 리다이렉트
+		return "redirect:/list";
+	}
+
+	// 전화번호 삭제
+	@RequestMapping(value = "/delete", method = { RequestMethod.GET, RequestMethod.POST })
+	public String delete(@RequestParam("no") int no) {
+		System.out.println("PhoneController>delete()");
+
+		// Service를 통해서 삭제한다
+		int count = phoneService.personDelete(no);
 
 		return "redirect:/list";
 	}
-	
-	//삭제
-	
-	@RequestMapping(value = "/delete", method = { RequestMethod.GET, RequestMethod.POST })
-	public String delete2(@RequestParam("no") int no) {
+
+	// 전화번호 등록(파라미터 사용)
+	@RequestMapping(value = "/write2", method = { RequestMethod.GET, RequestMethod.POST })
+	public String write2(@RequestParam("name") String name, @RequestParam("hp") String hp,
+			@RequestParam("company") String company) {
+
+		System.out.println("PhoneController>write2()");
 
 		// 파라미터 꺼내기
-		System.out.println(no);
+		/*
+		 * System.out.println(name); System.out.println(hp);
+		 * System.out.println(company);
+		 */
 
-		// 서비스로 저장
-		int count = phoneService.personDelete(no);
+		// vo로 묶기
+		PersonVo personVo = new PersonVo(name, hp, company);
+		System.out.println(personVo);
 
+		// Service를 통해서 저장한다
+		int count = phoneService.personInsert(personVo);
+		System.out.println(count);
+
+		// 리다이렉트
+		return "redirect:/list";
+	}
+
+	// 전화번호 삭제(@PathVariable 사용)
+	@RequestMapping(value = "/delete2/{no}", method = { RequestMethod.GET, RequestMethod.POST })
+	public String delete2(@PathVariable("no") int num) {
+		System.out.println("PhoneController>delete()");
+
+		// 주소에서 값 꺼내기
+		System.out.println(num);
+
+		// Service를 통해서 삭제한다
+		int count = phoneService.personDelete(num);
 		System.out.println(count);
 
 		return "redirect:/list";
@@ -86,14 +117,15 @@ public class PhoneController {
 	// 전화번호 수정폼
 	@RequestMapping(value = "/updateForm", method = { RequestMethod.GET, RequestMethod.POST })
 	public String updateForm(Model model, @RequestParam("no") int no) {
+		System.out.println("PhoneController>updateForm()");
 
 		// 주소에서 값 꺼내기
 		System.out.println(no);
 
-		// 서비스로 저장
+		// Service를 통해서 1명정보 가져오기
 		PersonVo personVo = phoneService.getPerson(no);
 
-		// ds데이터 보내기 --> request attribute에 넣는다
+		// ds 데이터보내기 -->request attribute에 넣는다
 		model.addAttribute("personVo", personVo);
 
 		return "updateForm";
@@ -101,15 +133,15 @@ public class PhoneController {
 
 	// 전화번호 수정
 	@RequestMapping(value = "/update", method = { RequestMethod.GET, RequestMethod.POST })
-	public String update(Model model, @ModelAttribute PersonVo personVo) {
+	public String updateForm(Model model, @ModelAttribute PersonVo personVo) {
+		System.out.println("PhoneController>update()");
 
+		// 파라미터 꺼내기+vo로 묶기를 DS해서 메소드의 파라미터로 보내준다
 		System.out.println(personVo);
 
-		
-		//서비스로 저장 
+		// Service를 통해서 수정하기
 		int count = phoneService.personUpdate(personVo);
-		System.out.println(count);
-		 
+
 		return "redirect:/list";
 	}
 
--- webapp/WEB-INF/views/updateForm.jsp
+++ webapp/WEB-INF/views/updateForm.jsp
@@ -1,5 +1,7 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,21 +9,20 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>수정</h1>
-	
+	<h1>전화번호부</h1>
 	<h2>수정폼</h2>
-	
-	<p>전화번호를 수정하려면<br>
-		아래항목을 기입하고 "수정" 버튼을 클릭하세요
-	</p>
-	
-	<form action="/phonebook4/update" method="get">
-		이름(name) <input type="text" name="name" value="${personVo}"><br>
-		핸드폰(hp) <input type="text" name="hp" value="${personVo}"><br>
-		회사(company) <input type="text" name="company" value="${personVo}"><br>
-		
+
+	<p>수정화면입니다. 아래의 항목을 수정하고 "수정"버튼을 클릭하세요</p>
+
+
+	<form action="./update" method="get">
+		이름(name) <input type="text" name="name" value="${personVo.name}">
+		<br> 핸드폰(hp) <input type="text" name="hp" value="${personVo.hp}">
+		<br> 회사(company) <input type="text" name="company"
+			value="${personVo.company}"> <br> pk(personId) <input
+			type="text" name="personId" value="${personVo.personId}"> <br>
 		<button type="submit">수정</button>
 	</form>
-	
+
 </body>
 </html>
